b98c37914534f0e778d6cadd5843a61e502dbac9
Ryan <ryan.hopkins@maine.edu>
2025-07-01 23:05:13 +0000

    Rebuild attempt

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+// tailwind.config.js
+export default {
+    darkMode: 'class',   // ← enables the JS “add/remove .dark” strategy
+    content: ['./index.html', './src/**/*.{js,ts}'],
+    theme: { extend: {} },
+    plugins: [],
+  };
+  
